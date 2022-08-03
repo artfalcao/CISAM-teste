0b1b29e941ead83466b903698a38b4e0e390c540
Arthur-Falcao <arthurmonteirofalcao1995@gmail.com>
2022-08-02 21:14:25 -0300

    Inicializando o servidor

---
+++ README.md
@@ -0,0 +1,3 @@
+## Etapas p/ criação do projeto
+1. npm init 
+2. npm install --save express
---
+++ index.js
@@ -0,0 +1,16 @@
+//imports
+const express = require('express')
+const app = express()
+
+/* Configurações da Aplicação */
+
+
+
+/* Configurações de Rotas */
+
+
+
+/* Inicialização do Servidor */
+app.listen(3000, () => {
+    console.log('Aplicação rodando na porta 3000!')
+})
