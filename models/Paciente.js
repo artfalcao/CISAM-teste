1e16550111612a4bf52b2506b307f036cca47dba
Arthur-Falcao <arthurmonteirofalcao1995@gmail.com>
2022-08-09 09:49:10 -0300

    Completando dados da model Paciente

--- models/Paciente.js
+++ models/Paciente.js
@@ -6,14 +6,82 @@ const Paciente = db.sequelize.define('paciente', {
         allowNull: false,
         primaryKey: true
     },
+    termoLGPD: {
+        type: db.Sequelize.CHAR(3),
+        allowNull: false
+     },     
+    prontuario:{
+        type: db.Sequelize.CHAR(11),
+        allowNull: false
+    },
     nome:{
         type: db.Sequelize.STRING,
         allowNull: false
     },
-    prontuario:{
+    nomeSocial: {
+        type: db.Sequelize.STRING,
+        allowNull: true
+    },
+    telefone: {
         type: db.Sequelize.CHAR(11),
         allowNull: false
-    }
+    }, 
+    dataNasc: {
+        type: db.Sequelize.DATEONLY,
+        allowNull: false
+    },
+    sexo: {
+        type: db.Sequelize.CHAR(1),
+        allowNull: false
+    },
+    nomeMae: {
+        type: db.Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: db.Sequelize.STRING,
+        allowNull: false
+    },
+	 cep: {
+        type: db.Sequelize.CHAR(9),
+        allowNull: false
+     },
+	 logradouro: {
+        type: db.Sequelize.STRING,
+        allowNull: false
+     },
+	 numero: {
+        type: db.Sequelize.STRING,
+        allowNull: false
+     },
+	 complemento: {
+        type: db.Sequelize.STRING,
+        allowNull: true
+     },
+	 bairro: {
+        type: db.Sequelize.STRING,
+        allowNull: false
+     },
+	 cidade: {
+        type: db.Sequelize.STRING,
+        allowNull: false
+     },
+     uf: {
+        type: db.Sequelize.STRING,
+        allowNull: false
+     },
+	 numeroSUS: {
+        type: db.Sequelize.STRING,
+        allowNull: false
+     },
+	 certidaoNasc: {
+        type: db.Sequelize.STRING,
+        allowNull: true
+     },
+	 certidaoCas: {
+        type: db.Sequelize.STRING,
+        allowNull: true
+     } 
 }) 
 
 Paciente.sync()
